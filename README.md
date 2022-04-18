d59b1443f02a96c41edf32894f91d325559118ac
artliou <>
2022-04-18 08:21:35 -0700

    Pushing Local Changes

--- README.md
+++ README.md
@@ -2,15 +2,27 @@
 
 Learning Socket.io from <https://socket.io/get-started/chat/>
 
-7/25/17 Code Editor: Atom Laptop: MacBook Pro
+7/25/17 
+
+Code Editor: Atom 
+
+Laptop: MacBook Pro
+
 
 Online Homework
+
 Here are some ideas to improve the application:
 
 Broadcast a message to connected users when someone connects or disconnects
+
 Add support for nicknames
+
 Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+
 Add “{user} is typing” functionality
+
 Show who’s online
+
 Add private messaging
+
 Share your improvements!
--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+// import express from 'express';
+// import webpack from 'webpack';
+// import path from 'path';
+// import config from '../webpack.config.dev';
+// import open from 'open';
+// import socket from 'socket.io'
+// import { Server } from 'http'
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -14,5 +22,5 @@ io.on('connection', function(socket) {
 });
 
 http.listen(port, function() {
-  console.log('listening on *:' + port );
+  console.log('listening on *:' + port);
 });
